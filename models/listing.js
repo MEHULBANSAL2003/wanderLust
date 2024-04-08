324376495d980986a401eba56040608f747c641d
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-08 13:16:49 +0530

    setting up and connecting and testing with mongo with sample data

---
+++ models/listing.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose");
+
+const Schema =mongoose.Schema;
+
+const ListingSchema= new Schema({
+ title:{
+    type:String,
+    required:true
+},
+ description:String,
+ image:{
+  type:String,
+  default:"https://plus.unsplash.com/premium_photo-1671229455003-5e7b9aaae0dc?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29jb251dCUyMHRyZWV8ZW58MHx8MHx8fDA%3D",
+  set:(v)=> v===""?"https://plus.unsplash.com/premium_photo-1671229455003-5e7b9aaae0dc?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29jb251dCUyMHRyZWV8ZW58MHx8MHx8fDA%3D":v,
+ },
+ price:Number,
+ location:String,
+ country:String
+
+
+})
+
+const Listing = mongoose.model("Listing",ListingSchema);
+
+module.exports=Listing;
