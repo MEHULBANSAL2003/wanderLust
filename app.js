4d9baa7476c0125e1ffa77d53951b59942d0ec98
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-08 17:05:52 +0530

    show route to print all the data of specific post when clicked

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path=require("path");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -50,8 +51,15 @@ async function main(){
     // res.send("working");
  });
 
+ // show route to print the data of clicked title
 
+ app.get("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+     const listing=await Listing.findById(id);
 
+     res.render("listings/show.ejs",{listing});
+
+ })
 
  app.listen(8080,()=>{
      console.log("server is listening to port 8080");
