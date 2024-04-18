914baed74e5b756edab5f2a47bc54842ddae7f4e
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-18 23:10:39 +0530

    adding routes for listing

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const wrapAsync=require("./utils/wrapAsync.js")
 const ExpressError=require("./utils/ExpressError.js")
  const {listingSchema,reviewSchema}=require("./schema.js");
  const Review=require("./models/review.js");
+ 
+ const listings=require("./routes/listing.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -37,20 +39,6 @@ async function main(){
      res.send("hi i m root");
  })
  
-// just creating fucntion for validating schema in middleware
-
-const validateListing=(req,res,next)=>{
-    let {error}= listingSchema.validate(req.body);  // it will check the data.. and if any field is missing it will give error
-    
-    if(error){  // if errorr is there we will throw it...!!
-        let errMsg=error.details.map((el)=> el.message).join(",");
-            console.log(errMsg);
-     throw new ExpressError(400,errMsg);
-    }
-    else{
-        next();
-    }
-}
 
 // function for server side validation of reviews
 const validateReview=(req,res,next)=>{
@@ -66,147 +54,7 @@ const validateReview=(req,res,next)=>{
     }
 }
 
-
- 
-
-//  app.get("/testListing",async (req,res)=>{
-//      let sampleListing=new Listing({
-//          title:"My New Villa",
-//          description:"By the Beach",
-//          price:1200,
-//          location:"Calangute,Goa",
-//          country: "  India"
-//      })
-//  await sampleListing.save();
-//  console.log("sample was saved");
-//  res.send("succesfull testing");
- 
-      
-//  })    
- 
- // INDEX ROUTE: get request on /listings..!!
- app.get("/listings", wrapAsync(async (req,res)=>{
-   const listings= await Listing.find({});
-   res.render("listings/index.ejs",{listings});
-
-    // res.send("working");
- }));
-
-
- // CREATE ROUTE.. for new listing
- app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs"); 
- })
-
- app.post("/listings",validateListing,wrapAsync(async (req,res,next)=>{
-
-   //validating server side using JOI
-//     let result= listingSchema.validate(req.body);  // it will check the data.. and if any field is missing it will give error
-//    console.log(result);
-//    if(result.error){  // if errorr is there we will throw it...!!
-//     throw new ExpressError(400,result.error);
-//    }
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,"Send valid data for listing")
-    // }
-    // // try{
-     let listing=req.body.listing;
-    // // now we will check fo individual keys whethere they are valid or not..!!
-    // if(!listing.title){
-    //     throw new ExpressError(400,"Title is missing")
-    // }
-    // if(!listing.description){
-    //     throw new ExpressError(400,"Description is missing")
-    // }
-    // if(!listing.location){
-    //     throw new ExpressError(400,"Location is missing")
-    // }
-    // if(!listing.price){
-    //     throw new ExpressError(400,"Price is missing")
-    // }
-    // if(!listing.country){
-    //     throw new ExpressError(400,"Country is missing")
-    // }
-
-
-    const newListing = {
-        title: listing.title,
-        description: listing.description,
-        image: {
-            filename: "listingimage",
-            url: listing.image
-        },
-        price: listing.price,
-        location: listing.location,
-        country: listing.country
-    };
-
-    const finalListing=new Listing(newListing);
-
-    await finalListing.save();
-    res.redirect("/listings");
-// }catch(err){
-//     next(err);
-// }
-    
-
-    //console.log(newListing);
-
- }));
-
- // edit route
- app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
- }));
-
- // put request
- app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>{
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,"Send valid data for listing")
-    // }
-    let {id}=req.params;
-    console.log(id);
-    let listing=req.body.listing;
-    
-    const newListing = {
-        title: listing.title,
-        description: listing.description,
-        image: {
-            filename: "listingimage",
-            url: listing.image
-        },
-        price: listing.price,
-        location: listing.location,
-        country: listing.country
-    };
-
-
-  const updatedList=await Listing.findByIdAndUpdate(id,newListing);
-
-  // Listing.findByIdAndUpdate(id,{newListing});
-
-   res.redirect("/listings");
-
- }));
-
- // show route to print the data of clicked title
-
- app.get("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-     const listing=await Listing.findById(id).populate("reviews");  // for printing actual review
-
-     res.render("listings/show.ejs",{listing});
-
- }));
-
- app.delete("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    const deletedListing=await Listing.findByIdAndDelete(id); // this line will automatically trigger mongoose middleware
-    console.log(deletedListing);
-    res.redirect("/listings");   
-}));
+app.use("/listings",listings);
 
 //REVIEWS..!!
 
---
+++ routes/listing.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync=require("../utils/wrapAsync.js")
+const {listingSchema,reviewSchema}=require("../schema.js");
+const ExpressError=require("../utils/ExpressError.js")
+const Listing=require("../models/listing.js");
+
+
+
+// just creating fucntion for validating schema in middleware
+
+const validateListing=(req,res,next)=>{
+    let {error}= listingSchema.validate(req.body);  // it will check the data.. and if any field is missing it will give error
+    
+    if(error){  // if errorr is there we will throw it...!!
+        let errMsg=error.details.map((el)=> el.message).join(",");
+            console.log(errMsg);
+     throw new ExpressError(400,errMsg);
+    }
+    else{
+        next();
+    }
+}
+
+
+// INDEX ROUTE: get request on /listings..!!
+router.get(
+  "/",
+  wrapAsync(async (req, res) => {
+    const listings = await Listing.find({});
+    res.render("listings/index.ejs", { listings });
+  })
+);
+
+// CREATE ROUTE.. for new listing
+router.get("/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+router.post(
+  "/",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    let listing = req.body.listing;
+
+    const newListing = {
+      title: listing.title,
+      description: listing.description,
+      image: {
+        filename: "listingimage",
+        url: listing.image,
+      },
+      price: listing.price,
+      location: listing.location,
+      country: listing.country,
+    };
+
+    const finalListing = new Listing(newListing);
+
+    await finalListing.save();
+    res.redirect("/listings");
+  })
+);
+
+// edit route
+router.get(
+  "/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+// put request
+router.put(
+  "/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    console.log(id);
+    let listing = req.body.listing;
+
+    const newListing = {
+      title: listing.title,
+      description: listing.description,
+      image: {
+        filename: "listingimage",
+        url: listing.image,
+      },
+      price: listing.price,
+      location: listing.location,
+      country: listing.country,
+    };
+
+    const updatedList = await Listing.findByIdAndUpdate(id, newListing);
+
+    // Listing.findByIdAndUpdate(id,{newListing});
+
+    res.redirect("/listings");
+  })
+);
+
+// show route to print the data of clicked title
+
+router.get(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews"); // for printing actual review
+
+    res.render("listings/show.ejs", { listing });
+  })
+);
+
+router.delete(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const deletedListing = await Listing.findByIdAndDelete(id); // this line will automatically trigger mongoose middleware
+    console.log(deletedListing);
+    res.redirect("/listings");
+  })
+);
+
+
+module.exports=router; 
