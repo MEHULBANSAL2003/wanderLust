740e7cfffd5bd1b4931e6ea932adde65dcefcf7f
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-19 00:11:59 +0530

    creating routes for review and using mergeParams

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
-const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate"); // for ejs styling.. templating   
-const wrapAsync=require("./utils/wrapAsync.js")
 const ExpressError=require("./utils/ExpressError.js")
- const {listingSchema,reviewSchema}=require("./schema.js");
- const Review=require("./models/review.js");
+
+ //const {listingSchema,reviewSchema}=require("./schema.js");
+ //const Review=require("./models/review.js");
+ //const wrapAsync=require("./utils/wrapAsync.js")
+ //const Listing=require("./models/listing.js");
  
  const listings=require("./routes/listing.js");
+ const reviews=require("./routes/review.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -28,64 +30,16 @@ main().then(()=>{
   console.log(err);
 })
 
-
 async function main(){
     await mongoose.connect(MONGO_URL);
  }
  
- 
-
  app.get("/", (req,res)=>{
      res.send("hi i m root");
  })
- 
-
-// function for server side validation of reviews
-const validateReview=(req,res,next)=>{
-    let {error}= reviewSchema.validate(req.body);  // it will check the data.. and if any field is missing it will give error
-    
-    if(error){  // if errorr is there we will throw it...!!
-        let errMsg=error.details.map((el)=> el.message).join(",");
-         console.log(errMsg);
-     throw new ExpressError(400,errMsg);
-    }
-    else{
-        next();
-    }
-}
 
 app.use("/listings",listings);
-
-//REVIEWS..!!
-
-// routes for reviews..!!
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async (req,res)=>{
-    let listing=await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    
-    console.log("review saved successfully");
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-
-// delete route for reviews
-
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
-  //  console.log("wroking");
-   let {id,reviewId}=req.params;
-   await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-  await Review.findByIdAndDelete(reviewId);
- res.redirect(`/listings/${id}`);
- 
-}));
-
-
-
+app.use("/listings/:id/reviews",reviews);
 
 app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page Not Found")); 
---
+++ routes/review.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router({mergeParams:true}); // mergeparams is method to use the parent params also..!!
+const ExpressError=require("../utils/ExpressError.js");
+const wrapAsync=require("../utils/wrapAsync.js");
+const Review=require("../models/review.js");
+const Listing=require("../models/listing.js");
+const {reviewSchema}=require("../schema.js");
+
+
+
+
+// function for server side validation of reviews
+const validateReview=(req,res,next)=>{
+    let {error}= reviewSchema.validate(req.body);  // it will check the data.. and if any field is missing it will give error
+    
+    if(error){  // if errorr is there we will throw it...!!
+        let errMsg=error.details.map((el)=> el.message).join(",");
+         console.log(errMsg);
+     throw new ExpressError(400,errMsg);
+    }
+    else{
+        next();
+    }
+}
+
+
+// routes for reviews..!!
+router.post("/",validateReview,wrapAsync(async (req,res)=>{
+    let listing=await Listing.findById(req.params.id);
+    let newReview=new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+    
+    console.log("review saved successfully");
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+
+// delete route for reviews
+
+router.delete("/:reviewId",wrapAsync(async(req,res)=>{
+  //  console.log("wroking");
+   let {id,reviewId}=req.params;
+   await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+  await Review.findByIdAndDelete(reviewId);
+ res.redirect(`/listings/${id}`);
+ 
+}));
+
+
+module.exports=router;
