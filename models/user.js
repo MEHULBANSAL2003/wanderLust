4d0ad183911ffae47e718663052582b5dbcb6e68
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-22 16:54:34 +0530

    adding user model to databse and using passport for auth

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+const passportLocalMongoose=require("passport-local-mongoose");
+
+// passporst local mongoose automatically create username and password and also add hash function and salting.. 
+// so we dont need to explicitly add username and password in our database
+
+const userSchema=new Schema({
+    email:{
+        type:String,
+        required:true
+    }
+});
+
+User.plugin(passportLocalMongoose);
+module.exports=mongoose.model("User",userSchema);
