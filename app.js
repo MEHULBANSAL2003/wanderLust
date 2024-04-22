8f63647a0e7a46e9378704235885ec1b1c075f32
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-23 01:34:18 +0530

    showing login, signup, logout on nabar

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ passport.deserializeUser(User.deserializeUser());  // to delete the info of user
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
+    res.locals.currUser=req.user;
     next();
 })
 
