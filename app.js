c3ae5781ec936d5bdb687667aef41899ad4b732b
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-22 18:48:16 +0530

    rendering signup page and using user routes for this

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ const User=require("./models/user.js");
  //const wrapAsync=require("./utils/wrapAsync.js")
  //const Listing=require("./models/listing.js");
  
- const listings=require("./routes/listing.js");
- const reviews=require("./routes/review.js");
+ const listingRouter=require("./routes/listing.js");
+ const reviewRouter=require("./routes/review.js");
+ const userRouter=require("./routes/user.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -74,8 +75,9 @@ app.use((req,res,next)=>{
     next();
 })
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);  // using user route
 
 app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page Not Found")); 
