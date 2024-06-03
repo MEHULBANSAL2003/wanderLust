1c1b780a36a0b232e716c5e71427c6ec8af138b2
Mehul Bansal <mehulbansal83606@gmail.com>
2024-06-03 12:12:27 +0530

    using mvc for listing

--- controllers/listings.js
+++ controllers/listings.js
@@ -4,3 +4,96 @@ module.exports.index=async (req, res) => {
     const listings = await Listing.find({});
     res.render("listings/index.ejs", { listings });
   }
+
+  module.exports.renderNewForm=(req, res) => {
+ 
+    res.render("listings/new.ejs");
+  }
+
+
+  module.exports.showListing=async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id).populate({path:"reviews",populate:{path:"author"}}).populate("owner"); // for printing actual review
+    if(!listing){
+      req.flash("error","Listing you requested for does not exists!");
+      res.redirect("/listings");
+    }
+    //console.log(listing);
+
+    res.render("listings/show.ejs", { listing });
+  }
+
+  module.exports.createListing=async (req, res, next) => {
+    let listing = req.body.listing;
+
+    const newListing = {
+      title: listing.title,
+      description: listing.description,
+      image: {
+        filename: "listingimage",
+        url: listing.image,
+      },
+      price: listing.price,
+      location: listing.location,
+      country: listing.country,
+    };
+
+    const finalListing = new Listing(newListing);
+     finalListing.owner=req.user._id;
+
+    await finalListing.save();
+    req.flash("success","new lisitng created successfully!"); // flash msg when new lisitng is created  
+    res.redirect("/listings");
+  }
+
+
+  module.exports.renderEditForm=async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if(!listing){
+      req.flash("error","Listing you requested for does not exists!");
+      res.redirect("/listings");
+    }
+    res.render("listings/edit.ejs", { listing });
+  }
+
+
+  module.exports.updateListing=async (req, res) => {
+    let { id } = req.params;
+   // console.log(id);
+    let listing = req.body.listing;
+    console.log(listing);
+    const newListing = {
+      title: listing.title,
+      description: listing.description,
+      image: {
+        filename: "listingimage",
+        url: listing.image,
+      },
+      price: listing.price,
+      location: listing.location,
+      country: listing.country,
+    };
+ 
+    listing=await Listing.findById(id);
+    // console.log(listing.owner);
+    // console.log(res.locals.currUser._id);
+    if(!listing.owner.equals(res.locals.currUser._id)){
+      req.flash("error","You don't have permission to edit");
+     return res.redirect(`/listings/${id}`);
+    }
+    const updatedList = await Listing.findByIdAndUpdate(id, newListing);
+
+    // Listing.findByIdAndUpdate(id,{newListing});
+    req.flash("success","Listing Updated Successfully!");
+
+    res.redirect("/listings");
+  }
+
+
+  module.exports.deleteListing=async (req, res) => {
+    let { id } = req.params;
+    const deletedListing = await Listing.findByIdAndDelete(id); // this line will automatically trigger mongoose middleware
+     req.flash("success","Listing deleted successfully!")
+    res.redirect("/listings");
+  }
--- routes/listing.js
+++ routes/listing.js
@@ -1,145 +1,45 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync=require("../utils/wrapAsync.js")
-const {listingSchema}=require("../schema.js");
-const ExpressError=require("../utils/ExpressError.js")
-const Listing=require("../models/listing.js");
-const {isLoggedIn}=require("../middleware.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const { listingSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, validateListing } = require("../middleware.js");
 
-const listingController=require("../controllers/listings.js")
-
-// just creating fucntion for validating schema in middleware
-
-const validateListing=(req,res,next)=>{
-    let {error}= listingSchema.validate(req.body);  // it will check the data.. and if any field is missing it will give error
-    
-    if(error){  // if errorr is there we will throw it...!!
-        let errMsg=error.details.map((el)=> el.message).join(",");
-            console.log(errMsg);
-     throw new ExpressError(400,errMsg);
-    }
-    else{
-        next();
-    }
-}
-
+const listingController = require("../controllers/listings.js");
 
 // INDEX ROUTE: get request on /listings..!!
-router.get(
-  "/",
-  wrapAsync(listingController.index)
-);
+router.get("/", wrapAsync(listingController.index));
 
 // CREATE ROUTE.. for new listing
-router.get("/new", isLoggedIn,(req, res) => {
- 
-  res.render("listings/new.ejs");
-});
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 router.post(
-  "/",isLoggedIn,
+  "/",
+  isLoggedIn,
   validateListing,
-  wrapAsync(async (req, res, next) => {
-    let listing = req.body.listing;
-
-    const newListing = {
-      title: listing.title,
-      description: listing.description,
-      image: {
-        filename: "listingimage",
-        url: listing.image,
-      },
-      price: listing.price,
-      location: listing.location,
-      country: listing.country,
-    };
-
-    const finalListing = new Listing(newListing);
-     finalListing.owner=req.user._id;
-
-    await finalListing.save();
-    req.flash("success","new lisitng created successfully!"); // flash msg when new lisitng is created  
-    res.redirect("/listings");
-  })
+  wrapAsync(listingController.createListing)
 );
 
 // edit route
 router.get(
-  "/:id/edit",isLoggedIn,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    if(!listing){
-      req.flash("error","Listing you requested for does not exists!");
-      res.redirect("/listings");
-    }
-    res.render("listings/edit.ejs", { listing });
-  })
+  "/:id/edit",
+  isLoggedIn,
+  wrapAsync(listingController.renderEditForm)
 );
 
 // put request
 router.put(
-  "/:id",isLoggedIn,
+  "/:id",
+  isLoggedIn,
   validateListing,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-   // console.log(id);
-    let listing = req.body.listing;
-    console.log(listing);
-    const newListing = {
-      title: listing.title,
-      description: listing.description,
-      image: {
-        filename: "listingimage",
-        url: listing.image,
-      },
-      price: listing.price,
-      location: listing.location,
-      country: listing.country,
-    };
- 
-    listing=await Listing.findById(id);
-    // console.log(listing.owner);
-    // console.log(res.locals.currUser._id);
-    if(!listing.owner.equals(res.locals.currUser._id)){
-      req.flash("error","You don't have permission to edit");
-     return res.redirect(`/listings/${id}`);
-    }
-    const updatedList = await Listing.findByIdAndUpdate(id, newListing);
-
-    // Listing.findByIdAndUpdate(id,{newListing});
-    req.flash("success","Listing Updated Successfully!");
-
-    res.redirect("/listings");
-  })
+  wrapAsync(listingController.updateListing)
 );
 
 // show route to print the data of clicked title
 
-router.get(
-  "/:id",
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id).populate({path:"reviews",populate:{path:"author"}}).populate("owner"); // for printing actual review
-    if(!listing){
-      req.flash("error","Listing you requested for does not exists!");
-      res.redirect("/listings");
-    }
-    //console.log(listing);
+router.get("/:id", wrapAsync(listingController.showListing));
 
-    res.render("listings/show.ejs", { listing });
-  })
-);
+router.delete("/:id", isLoggedIn, wrapAsync(listingController.deleteListing));
 
-router.delete(
-  "/:id",isLoggedIn,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const deletedListing = await Listing.findByIdAndDelete(id); // this line will automatically trigger mongoose middleware
-     req.flash("success","Listing deleted successfully!")
-    res.redirect("/listings");
-  })
-);
-
-
-module.exports=router; 
+module.exports = router;
