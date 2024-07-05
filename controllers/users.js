8f714f0837212d34e75caeaf0c1d704938d8f1bf
Mehul Bansal <mehulbansal83606@gmail.com>
2024-07-06 00:59:40 +0530

    post-login page

--- controllers/users.js
+++ controllers/users.js
@@ -31,9 +31,9 @@ module.exports.signup=async (req, res) => {
 
   module.exports.login=async (req, res) => {
     req.flash("success", "Welcome back to WanderLust! You are logged in!");
-    let redirectUrl="/listings";
-    console.log(redirectUrl);
-    res.redirect(redirectUrl);
+    // let redirectUrl="/listings";
+    let redirectUrl=res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
   }
 
   module.exports.logout=(req, res) => {
